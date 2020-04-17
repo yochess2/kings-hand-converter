1b16c250ea11dd59668e348318d08ea792e6febb
Daham Wang <yochess@Dahams-MacBook-Pro.local>
2020-04-17 08:00:57 -0700

    fix async and naming issues

--- dev/dev_html_parser.js
+++ dev/dev_html_parser.js
@@ -7,29 +7,68 @@ const folder = './onc_grabber/'
 const folder_2 = './onc_grabber_done/'
 const converter_onc = require('../extension/converter/converter_onc.js')
 
-files.forEach((file, fileIndex) => {
-  if (file === '.DS_Store') {
-    return
-  }
-  let fileStr = ''
-  let filename = `filename_${fileIndex}`
-  getHand(file).forEach((oldHandText, handIndex) => {
-    if (oldHandText === "") {
-      return
+process(files)
+
+async function process(files) {
+  let fileIndex = 0
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i]
+    console.log(`1. File # ${fileIndex}: ${file}`)
+    if (file === '.DS_Store') {
+      fileIndex++
+      continue
     }
-    // if (handIndex === 1) {
+    let fileStr = ''
+    // let filename = `filename_${fileIndex}`
+    let filename = `${file}_converted`
+    let HHs = await getHand(file)
+    let handIndex = 0
+    for (let j = 0; j < HHs.length; j++) {
+      let oldHandText = HHs[j]
+      if (oldHandText === "") {
+        handIndex++
+        continue
+      }
       let oldHand = filterHand(oldHandText)
       if (oldHand.lines.length <= 11) {
-        return
+        handIndex++
+        continue
       }
-      const newHand = converter_onc.convertOnc(oldHand, true)
+      const newHand = converter_onc.convertOnc(oldHand, false)
       if (newHand.display) {
         fileStr += newHand.text + '\n'
       }
-    // }
-  })
-  // saveFile(folder_2, filename, fileStr)
-})
+      handIndex++
+    }
+    await saveFile(folder_2, filename, fileStr)
+    fileIndex++
+  }
+
+
+  // files.forEach((file, fileIndex) => {
+  //   if (file === '.DS_Store') {
+  //     return
+  //   }
+  //   let fileStr = ''
+  //   let filename = `filename_${fileIndex}`
+  //   getHand(file).forEach((oldHandText, handIndex) => {
+  //     if (oldHandText === "") {
+  //       return
+  //     }
+  //     // if (handIndex === 1) {
+  //       let oldHand = filterHand(oldHandText)
+  //       if (oldHand.lines.length <= 11) {
+  //         return
+  //       }
+  //       const newHand = converter_onc.convertOnc(oldHand, true)
+  //       if (newHand.display) {
+  //         fileStr += newHand.text + '\n'
+  //       }
+  //     // }
+  //   })
+    // await saveFile(folder_2, filename, fileStr)
+  // })
+}
 
 function populateOldHand(text) {
   return {
@@ -39,7 +78,7 @@ function populateOldHand(text) {
   }
 }
 
-function getHand(file) {
+async function getHand(file) {
   const text = fs.readFileSync(path.resolve(__dirname, folder, file), 'utf8')
   return text.split('EOH')
 }
@@ -89,9 +128,13 @@ function filterHand(oldHandText) {
   return hand
 } 
 
-function saveFile(folder_2, filename, fileStr) {
-  fs.writeFile(`${folder_2}${filename}.txt`, fileStr, function (err) {
-    if (err) return console.log(err);
-    // console.log(`${filename} was a success!`);
-  })
+async function saveFile(folder_2, filename, fileStr) {
+  fs.writeFileSync(`${folder_2}${filename}.txt`, fileStr)
+  console.log(`${filename} was a success!`)
+  return
+
+  // await fs.writeFile(`${folder_2}${filename}.txt`, fileStr, function (err) {
+  //   if (err) return console.log(err);
+  //   return console.log(`${filename} was a success!`);
+  // })
 }
