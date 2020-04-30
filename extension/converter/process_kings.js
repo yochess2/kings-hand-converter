4cbe963f62a9c0a24d0d9cc004a300b6dfd8023f
Daham Wang <yochess@Dahams-MacBook-Pro.local>
2020-04-30 11:54:34 -0700

    moduralize

--- extension/converter/process_kings.js
+++ extension/converter/process_kings.js
@@ -21,59 +21,10 @@
       it.isStop = true
     }
     await loopIt(archiveHandElems, it, htmlElems, 0)
-
-    const elem_fiddy = document.getElementsByClassName('style_button style_banner_button')[0]
-    let elem_hand_count = document.getElementsByClassName('style_status_bar_pane')[1]
-    let hand_count = elem_hand_count.innerHTML
-    let outer_counter = 0
-    let last_index = archiveHandElems.length
-    if ((autoClick === 'yes') && (!(it.error)) && (outer_counter < 1000)) {
-      while (!(isLastHand(it.endNum, it.currentNum)) && !(it.isStop)) {
-        let inner_counter = 0
-        hand_count = elem_hand_count.innerHTML // 50
-        elem_fiddy.click()
-        while ((hand_count === elem_hand_count.innerHTML) && (inner_counter < a.timeToDelay) && (!(it.isStop))) {
-          elem_hand_count = document.getElementsByClassName('style_status_bar_pane')[1]
-          htmlElems.lag.innerHTML = `${parseInt(inner_counter/100)}/${a.timeToDelay/100}`
-          await h.delay(1)
-          inner_counter++
-        }
-        if (inner_counter <= a.timeToDelay) {
-          htmlElems.hc.innerHTML = archiveHandElems.length
-          await h.delay(1)
-          it.looped = false
-          await loopIt(archiveHandElems, it, htmlElems, last_index)
-          last_index += 50
-          if (!(it.looped)) {
-            h.delay(2000)
-          }
-        } else {
-          htmlElems.lag.innerHTML = `TIME OUT`
-          break
-        }
-        outer_counter++
-      }
+    if ((autoClick === 'yes') && (!(it.error))) {
+      await plusFiddy(archiveHandElems, it, htmlElems)
     }
-
-    const cBtn = document.createElement("BUTTON")
-    const uBtn = document.createElement("BUTTON")
-    const hcBtn = document.createElement("BUTTON")
-    cBtn.innerHTML = "Download"
-    uBtn.innerHTML = "Download"
-    hcBtn.innerHTML = "Download"
-    htmlElems.c.appendChild(cBtn)
-    htmlElems.u.appendChild(uBtn)
-    htmlElems.hc.appendChild(hcBtn)
-
-    cBtn.onclick = (stuff) => {
-      chrome.runtime.sendMessage({text: 'download', str: it.convertedStr})
-    }
-    uBtn.onclick = (stuff) => {
-      chrome.runtime.sendMessage({text: 'download', str: it.unConvertedStr})
-    }
-    hcBtn.onclick = (stuff) => {
-      chrome.runtime.sendMessage({text: 'download', str: it.originalStr})
-    }
+    createButtons(htmlElems, it)
   }
 
   async function loopIt(archiveHandElems, it, htmlElems, lastIndex) {
@@ -83,7 +34,6 @@
       if (!(validHand(archiveHandElem))) { continue }
       it.currentNum = getCurrentArchiveHandNum(archiveHandElem)
       if (duplicateHand(it.currentNum, it.beforeNum)) {
-        console.log('>>', it.currentNum, it.beforeNum, lastIndex)
         htmlElems.d.innerHTML = parseInt(htmlElems.d.innerHTML) + 1
         continue
       }
@@ -115,6 +65,60 @@
     }
   }
 
+  async function plusFiddy(archiveHandElems, it, htmlElems) {
+    const elem_fiddy = document.getElementsByClassName('style_button style_banner_button')[0]
+    let elem_hand_count = document.getElementsByClassName('style_status_bar_pane')[1]
+    let hand_count = elem_hand_count.innerHTML
+    let outer_counter = 0
+    let last_index = archiveHandElems.length
+    while (!(isLastHand(it.endNum, it.currentNum)) && !(it.error) && !(it.isStop)) {
+      let inner_counter = 0
+      hand_count = elem_hand_count.innerHTML // 50
+      elem_fiddy.click()
+      while ((hand_count === elem_hand_count.innerHTML) && (!(it.isStop)) && !(it.error)) {
+        elem_hand_count = document.getElementsByClassName('style_status_bar_pane')[1]
+        htmlElems.lag.innerHTML = `${parseInt(inner_counter/100)}/${a.timeToDelay/100}`
+        if (inner_counter > a.timeToDelay) {
+          it.error = true
+          htmlElems.lag.innerHTML = `TIME OUT`
+        }
+        await h.delay(1)
+        inner_counter++
+      }
+      htmlElems.hc.innerHTML = archiveHandElems.length
+      await h.delay(1)
+      it.looped = false
+      await loopIt(archiveHandElems, it, htmlElems, last_index)
+      last_index += 50
+      if (!(it.looped)) {
+        h.delay(2000)
+      }
+      outer_counter++
+    }
+  }
+
+  function createButtons(htmlElems, it) {
+    const cBtn = document.createElement("BUTTON")
+    const uBtn = document.createElement("BUTTON")
+    const hcBtn = document.createElement("BUTTON")
+    cBtn.innerHTML = "Download"
+    uBtn.innerHTML = "Download"
+    hcBtn.innerHTML = "Download"
+
+    htmlElems.c.appendChild(cBtn)
+    htmlElems.u.appendChild(uBtn)
+    htmlElems.hc.appendChild(hcBtn)
+
+    cBtn.onclick = (stuff) => {
+      chrome.runtime.sendMessage({text: 'download', str: it.convertedStr})
+    }
+    uBtn.onclick = (stuff) => {
+      chrome.runtime.sendMessage({text: 'download', str: it.unConvertedStr})
+    }
+    hcBtn.onclick = (stuff) => {
+      chrome.runtime.sendMessage({text: 'download', str: it.originalStr})
+    }
+  }
 
     ////// Helper //////
    /* processArchive */
